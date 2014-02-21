dbb47999ec35aeb2d16df1e56e4f6978c4a6484f
David Tsuji <dts@sitecore.net>
2014-02-21 15:19:49 +1000

    first

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+  grunt: {
+    jshint: {
+      "curly": true,
+      "eqeqeq": true,
+      "immed": false,
+      "latedef": true,
+      "quotmark": "double",
+      "noarg": true,
+      "forin": true,
+      "newcap": true,
+      "sub": true,
+      "undef": false,
+      "boss": true,
+      "strict": false,
+      "unused": false,
+      "eqnull": true,
+      "node": true,
+      "browser": true,
+      "expr": "warn"
+    }
+  }
+
+}
